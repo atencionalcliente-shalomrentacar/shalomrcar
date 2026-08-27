78f0a603e5713607242598975c7a0e22f1682a89
atencionalcliente-shalomrentacar <atencionalcliente@shalomrentacar.com>
2026-08-27 09:12:19 -0600

    newconetcion

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBlO9ULEIETH02LOx3NVPnMCkQCG14nB0U",
+  authDomain: "shalomrcarmant.firebaseapp.com",
+  databaseURL: "https://shalomrcarmant-default-rtdb.firebaseio.com",
+  projectId: "shalomrcarmant",
+  storageBucket: "shalomrcarmant.firebasestorage.app",
+  messagingSenderId: "78788139942",
+  appId: "1:78788139942:web:8f0097f1143f085f960d67",
+  measurementId: "G-X2CC17QNH3"
+};
